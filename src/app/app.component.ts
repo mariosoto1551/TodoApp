687ffaa5833a6c48f92e3fd77299ff15b9fcab9b
Jose Rodriguez <mariosoto1551@gmail.com>
2024-08-24 12:21:27 -0400

    Inicio de app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardLayoutComponent } from './presentation/layouts/dashboardLayout/dashboardLayout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './dashboardLayout.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardLayoutComponent { }
